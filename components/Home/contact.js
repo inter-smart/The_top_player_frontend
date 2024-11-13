825acf07a3c04d8771dec546a56b0dfd2d77ca81
Vaishnav-Vijayan07 <vaishnav@intersmart.in>
2024-11-13 19:11:25 +0530

    removed text underline

--- components/Home/contact.js
+++ components/Home/contact.js
@@ -136,11 +136,10 @@ const Contact = ({ styles, Lang }) => {
                         rel="noopener noreferrer"
                         className={styles.span_div}
                       >
-                        <A style={{ textDecoration: "none" }} iFillInstagram />
+                        <AiFillInstagram />
                       </a>
 
-                      <a
-                        style={{ textDecoration: "none" }}
+                      <a style={{ textDecoration: "none" }}
                         aria-label="our whatsapp "
                         href={
                           "https://api.whatsapp.com/send/?phone=971501225632&text=%D9%85%D8%B1%D8%AD%D8%A8%D8%A7%2C+%D8%B9%D9%86%D8%AF%D9%8A+%D8%A7%D8%B3%D8%AA%D9%81%D8%B3%D8%A7%D8%B1+%D8%A8%D8%AE%D8%B5%D9%88%D8%B5&type=phone_number&app_absent=0"
